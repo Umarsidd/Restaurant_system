0331c6080aebdffad778bc16d89ddc4fe88faf21
Umarsidd <mohdumar4253@gmail.com>
2025-12-28 21:13:25 +0530

    Use standard DJANGO_SETTINGS_MODULE for production config

--- config/wsgi.py
+++ config/wsgi.py
@@ -11,11 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-# Debug: Check environment variable
-django_env = os.environ.get('DJANGO_ENV', 'not-set')
-settings_module = 'config.production_settings' if django_env == 'production' else 'config.settings'
-print(f"🔍 DJANGO_ENV={django_env}, Using settings: {settings_module}")
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+# Use production settings by default
+# Override with DJANGO_SETTINGS_MODULE env var if needed
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.production_settings')
 
 application = get_wsgi_application()
